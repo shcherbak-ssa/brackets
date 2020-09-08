758dd2cbbbe5105aea523428cf4a05df88fe6c20
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-09-08 23:54:06 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const removeClosedBrackets = createClosedBracketsRemover(bracketsConfig);
+  return recursionCheck(str);
+
+  function recursionCheck(initialString) {
+    const stringLeft = removeClosedBrackets(initialString);
+    switch (stringLeft) {
+      case '': return true;
+      case initialString: return false;
+      default: return recursionCheck(stringLeft);
+    }
+  }
+}
+
+function createClosedBracketsRemover(bracketsConfig) {
+  bracketsConfig = bracketsConfig.map((config) => config.join(''));
+  return (str) => bracketsConfig.reduce((stringLeft, bracketConfig) => stringLeft.replace(bracketConfig, ''), str);
 }
